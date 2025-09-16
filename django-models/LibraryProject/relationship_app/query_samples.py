2ad38d38658da373867415ead213b32f4b046389
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-09-16 23:13:11 +0100

    change query sample

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,32 +1,16 @@
 from relationship_app.models import Author, Book, Library, Librarian
-# Sample Queries
 
 def get_books_by_author(author_name):
-	try:
-		author = Author.object.get(name=author_name)
-		books = author.books.all()
-		print(f"Books by {author_name}: {[book.title for book in books]}")
-	except Author.DoesNotExist:
-		print(f"No author found with name {author_name}")
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)    
+    return books
 
+# List all books in a library
 def get_books_in_library(library_name):
-	try:
-		library = Library.objects.get(name=library_name)
-		books = library.books.all()
-		print(f"Books in {library_name}: {[book.title for book in books]}")
-	except Library.DoesNotExist:
-		print(f"No library found with name {library_name}")
+    library = Library.objects.get(name=library_name)
+    return library.books.all()
 
+# Retrieve the librarian for a library
 def get_librarian_for_library(library_name):
-	try:
-		library = Library.objects.get(name=library_name)
-		librarian = library.librarian
-		print(f"Librarian for {library_name}: {librarian.name}")
-	except Library.DoesNotExist:
-		print(f"No library found with name {library_name}")
-
-if __name__ == "__main__":
-    # Example usage
-    get_books_by_author("J.K. Rowling")
-    get_books_in_library("Central Library")
-    get_librarian_for_library("Central Library")
+    library = Library.objects.get(name=library_name)
+    return Librarian.objects.get(library=library)
