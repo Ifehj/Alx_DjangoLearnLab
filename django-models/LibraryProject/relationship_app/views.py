9b026363aa3e0a6cdee2c9912c8c8bc3e5dd88ae
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-09-17 11:45:55 +0100

    role based access

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
 from .models import Book
 from .models import Library
 # Create your views here.
@@ -30,4 +31,24 @@ def register(request):
     return render(request, "relationship_app/register.html", {"form": form})
 
 
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
