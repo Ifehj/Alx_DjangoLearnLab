2ba42ce71508fec4a436cebea9465423b5ea2ed8
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-09-17 10:51:18 +0100

    update code

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
 from .views import list_books
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),  # function-based view
     path("library/<int:pk>/", views.LibraryDetailView.as_view(), name="library_detail"),  # class-based view
-	path("login/", views.user_login, name="login"),
-    path("logout/", views.user_logout, name="logout"),
+	path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,25 +17,6 @@ class LibraryDetailView(DetailView):
 	template_name = 'relationship_app/library_detail.html'
 	context_object_name = 'library'
 
-# User Login View
-def user_login(request):
-    if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("list_books")  # redirect after login
-    else:
-        form = AuthenticationForm()
-    return render(request, "relationship_app/login.html", {"form": form})
-
-
-# User Logout View
-def user_logout(request):
-    logout(request)
-    return render(request, "relationship_app/logout.html")
-
-
 # User Registration View
 def register(request):
     if request.method == "POST":
