036dbf1f062a5c65c98cf9f112314ba3bef5670c
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-06 19:18:13 +0100

    refactor: model

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class SimpleModel(models.Model):
+class Book(models.Model):
 	title = models.CharField(max_length=100, null=True)
 	author = models.CharField(max_length=100, null=True)
