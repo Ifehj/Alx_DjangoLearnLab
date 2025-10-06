4f08cc615df09d32bf4a78aaeb51a31f5f6d138b
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-06 20:12:46 +0100

    implement: authentication and permissions

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -10,7 +10,6 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     # Route for the BookList view (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
-
     path('', include(router.urls)), 
 ]
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,12 +3,16 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 # Create your views here.
 
 class BookList(generics.ListAPIView):
+	permission_classes = [permissions.AllowAny]
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
+	permission_classes = [permissions.IsAuthenticated]
+
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
