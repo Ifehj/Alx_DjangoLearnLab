ad58a4c9058d89bc409700a770d0222ff84d120e
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-08 16:29:09 +0100

    initial commit

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+	name = models.CharField(max_length=100, null=True)
+
+	def __str__(self):
+		return self.name
+
+class Book(models.Model):
+	title = models.CharField(max_length=100, null=True)
+	publication_year = models.IntegerField(null=True)
+	author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f"{self.title} ({self.publication_year})"
