ba09b53d3c2345e91bff78f454cd6355f4dba9cd
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-10 14:28:18 +0100

    feat: configure views and permissions

--- advanced_api_project/api/views.py
+++ advanced_api_project/api/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from rest_framework import generics, permissions
+from .models import Book
+from .serializers import BookSerializer
 # Create your views here.
+
+class ListView(generics.ListAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [permissions.AllowAny]
+
+	filter_backends = [filter.SearchFilter]
+	search_fields = ['title', 'author_name']
+
+class DetailView(generics.RetrieveAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [permissions.AllowAny]
+
+class CreateView(generics.CreateAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [permissions.IsAuthenticated]
+
+class UpdateView(generics.UpdateAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [permissions.IsAuthenticated]
+class DeleteView(generics.DestroyAPIView):
+	queryset = Book.objects.all()
+	serializer_class = BookSerializer
+	permission_classes = [permissions.IsAuthenticated]
