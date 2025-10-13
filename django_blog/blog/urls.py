522e4481756a5e82dc1e66f3a6a9fbe16274d701
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-13 20:45:51 +0100

    refactor: url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
 	path('posts/', views.PostListView.as_view(), name='posts-list'),
     path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
     path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
 	path('', views.home, name='home'),
 	path('register/', views.register, name='register'),
