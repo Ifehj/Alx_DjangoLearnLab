fbce8fb1141dda6574e48f524fe1aee537cf0013
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-13 20:48:03 +0100

    refactor: url

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('posts/', views.PostListView.as_view(), name='posts-list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 
