962258a602b7f8387306e796116082c873fa0a8f
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-21 17:04:55 +0100

    initial commit

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+	model = CustomUser
+	list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
+	list_filter = ('is_staff', 'is_active')
+	ordering = ('email',)
+	search_fields = ('email',)
+	fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
