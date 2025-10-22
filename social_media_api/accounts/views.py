b1dfdc39e3e10196f0aca16f2aa1cd1bc70b97d7
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-22 15:34:39 +0100

    change model field

---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.contrib.auth import login
+
+from .models import CustomUser
+from .serializers import RegisterSerializer, UserSerializer, LoginSerializer
+from rest_framework.response import Response
+# Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+	queryset = CustomUser.objects.all()
+	serializer_class = RegisterSerializer
+	
+	def create(self, request, *args, **kwargs):
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		user = serializer.save()
+		token, created = Token.objects.get_or_create(user=user)
+		return Response({
+			"user": UserSerializer(user).data,
+			"token": token.key
+		}, status=status.HTTP_201_CREATED)
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+        login(request, user)
+        return Response({
+            "user": UserSerializer(user).data,
+            "token": token.key
+        })
