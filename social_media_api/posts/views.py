bb938bee1027c84dfda4d150ca411d882fb6117b
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-22 23:25:12 +0100

    fix: alx checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -26,7 +26,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
 
 class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.select_related('author', 'post').all()
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     pagination_class = StandardResultsSetPagination
