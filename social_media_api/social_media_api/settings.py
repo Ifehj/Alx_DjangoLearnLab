ecb25fb66dbcde4211433de272abbfcd6ac03817
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-24 17:24:36 +0100

    set debug setting

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y8u&uo$@e(#)q=x!_=kjexz5dgjog%#c&xr8@*n-of0cjnu#_+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
