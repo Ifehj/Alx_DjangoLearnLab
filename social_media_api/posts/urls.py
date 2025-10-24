3f2adf00b20b1e5364a42a9de3ff0aa586219a13
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-10-24 17:09:09 +0100

    fix: alx error

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,12 @@ from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
 from django.urls import path
 
+
+urlpatterns = [
+    path('<int:pk>/like/', PostViewSet.as_view({'post': 'like'}), name='post-like'),
+    path('<int:pk>/unlike/', PostViewSet.as_view({'post': 'unlike'}), name='post-unlike'),
+]
+
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
